77b97d89d84f53b8b47678acca64ccf8242c6582
Andrei <anderson.andrei567@gmail.com>
2021-04-16 20:24:38 -0300

    create

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,4 +2,10 @@ class TasksController < ApplicationController
     def index
         @tasks = Task.all
     end
+
+    def create
+        get_task = params.require(:task).permit :name, :description, :priority
+
+        task = Task.create get_task
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 # Neste arquivo é definido qual controller estará responsável por determinada url
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  post "tasks" => "tasks#create"
   root "tasks#index"
 end
